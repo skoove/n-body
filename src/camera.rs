05023ee9ffcd9270dd15622f1b34a762da1a19e9
skoove <zie@skoove.dev>
2025-02-19 14:40:51 +1000

    implemented camera zooming

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,9 @@
+use bevy::input::mouse::MouseScrollUnit::{Line, Pixel};
+use bevy::input::mouse::MouseWheel;
+
 use crate::*;
+
+const ZOOM_SENSITIVITY: f32 = 0.1;
 
 pub struct CameraPlugin;
 
@@ -8,7 +13,7 @@ struct Camera;
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup_camera);
-        app.add_systems(Update, pan_camera);
+        app.add_systems(Update, zoom_camera);
     }
 }
 
@@ -16,8 +21,16 @@ fn setup_camera(mut commands: Commands) {
     commands.spawn((Camera, Camera2d, Transform::default()));
 }
 
-fn pan_camera(mut cam_q: Query<(&mut Transform, &mut OrthographicProjection)>) {
-    let (mut transform, mut camera) = cam_q.single_mut();
-    transform.translation += Vec3::new(0.0, 0.0, 0.0);
-    camera.scale -= 0.1;
+fn zoom_camera(
+    mut cam_q: Query<&mut OrthographicProjection>,
+    mut mouse_scroll_events: EventReader<MouseWheel>,
+) {
+    let mut projection = cam_q.single_mut();
+    for event in mouse_scroll_events.read() {
+        match event.unit {
+            Line => projection.scale += -event.y * ZOOM_SENSITIVITY,
+            Pixel => projection.scale += -event.y * ZOOM_SENSITIVITY,
+        }
+        projection.scale = projection.scale.max(0.0);
+    }
 }
