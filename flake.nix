1ae628ee359dc04823a678f00cfc4e2ac798fb83
Zie Sturges <53106860+skoove@users.noreply.github.com>
2025-03-20 14:16:23 +1000

    added half working flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [ pkgs.wayland pkgs.pkg-config ];
+      
+      packages = with pkgs; [
+        cargo
+        clippy
+        clang
+        lld
+        alsa-lib
+      ];
+    };
+  };
+}
