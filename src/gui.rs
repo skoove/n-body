724963d1b28cbcb874848ebafb9faf555fe94cac
skoove <zie@skoove.dev>
2025-03-21 21:00:41 +1000

    gui: stop inputs going through egui windows
    Grabbed some code from [1]. This fixes doing things like panning or
    zooming while mouse is over an egui window still doing that action.
    
    [1]: https://github.com/vladbat00/bevy_egui/issues/47#issuecomment-2368811068

--- src/gui.rs
+++ src/gui.rs
@@ -1,3 +1,4 @@
+use bevy::input::mouse::MouseWheel;
 use bevy_egui::{
     egui::{self, Ui},
     EguiContexts, EguiPlugin,
@@ -9,7 +10,15 @@ pub struct GuiPlugin;
 
 impl Plugin for GuiPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(EguiPlugin).add_systems(Update, preformance_gui);
+        app
+            .add_plugins(EguiPlugin)
+            .add_systems(Update, preformance_gui)
+            .add_systems(
+                PreUpdate, 
+                absorb_egui_inputs
+                    .after(bevy_egui::input::write_egui_input_system)
+                    .before(bevy_egui::begin_pass_system),
+            );
     }
 }
 
@@ -24,4 +33,38 @@ fn fps_widget(ui: &mut Ui, time: Res<Time>) {
     ui.label(format!("frametime: {:.1}ms", 1000.0 * time.delta_secs()));
     ui.label(format!("fps: {:.0}", 1.0 / time.delta_secs()));
     ui.separator();
+}
+
+// source:
+// https://github.com/vladbat00/bevy_egui/issues/47#issuecomment-2368811068
+fn absorb_egui_inputs(
+    mut contexts: bevy_egui::EguiContexts,
+    mut mouse: ResMut<ButtonInput<MouseButton>>,
+    mut mouse_wheel: ResMut<Events<MouseWheel>>,
+    mut keyboard: ResMut<ButtonInput<KeyCode>>,
+) {
+    let ctx = contexts.ctx_mut();
+    if !(ctx.wants_pointer_input() || ctx.is_pointer_over_area()) {
+        return;
+    }
+    let modifiers = [
+        KeyCode::SuperLeft,
+        KeyCode::SuperRight,
+        KeyCode::ControlLeft,
+        KeyCode::ControlRight,
+        KeyCode::AltLeft,
+        KeyCode::AltRight,
+        KeyCode::ShiftLeft,
+        KeyCode::ShiftRight,
+    ];
+
+    let pressed = modifiers.map(|key| keyboard.pressed(key).then_some(key));
+
+    mouse.reset_all();
+    mouse_wheel.clear();
+    keyboard.reset_all();
+
+    for key in pressed.into_iter().flatten() {
+        keyboard.press(key);
+    }
 }
