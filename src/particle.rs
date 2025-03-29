f7635a627ed665a5a717462fdfc0329b11139948
Zie Sturges <zie@sturges.com.au>
2025-03-29 10:22:00 +1000

    particle: remove all references to velocity

--- src/particle.rs
+++ src/particle.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 use rand::prelude::*;
 use std::f32::consts::PI;
 
-use crate::{Acceleration, Velocity};
+use crate::Acceleration;
 
 pub struct ParticlePlugin;
 
@@ -40,7 +40,6 @@ fn show_particles(
 
 fn spawn_random_particles(mut commands: Commands) {
     let amount_to_spawn = 50;
-    let velocity_range = -100.0..100.0;
     let mut rng = rand::rng();
     for _ in 0..amount_to_spawn {
         let angle: f32 = rng.random_range(0.0..2.0 * PI);
@@ -48,14 +47,10 @@ fn spawn_random_particles(mut commands: Commands) {
 
         let x = radius * angle.cos();
         let y = radius * angle.sin();
-
-        let x_v = rng.random_range(velocity_range.clone());
-        let y_v = rng.random_range(velocity_range.clone());
         commands.spawn((
             Particle { radius: 2.0 },
             Transform::from_xyz(x, y, 0.0),
             Mass(10.0),
-            Velocity(Vec2::new(x_v, y_v)),
             Acceleration(Vec2::new(0.0, 0.0)),
         ));
     }
@@ -66,7 +61,6 @@ fn spawn_big_particle(mut commands: Commands) {
         Particle { radius: 5.0 },
         Transform::from_xyz(0.0, 0.0, 0.0),
         Mass(250.0),
-        Velocity(Vec2::new(0.0, 0.0)),
         Acceleration(Vec2::new(0.0, 0.0)),
     ));
 }
