2c9be5f3ac67f16eb0fe298bb1e1f2906c8ea937
skoove <zie@skoove.dev>
2025-03-30 17:37:56 +1000

    i promise the commit messages will get good soon

--- src/gui/performance_gui.rs
+++ src/gui/performance_gui.rs
@@ -14,6 +14,7 @@ impl Plugin for PreformanceGuiPlugin {
         app.add_systems(Update, performance_gui)
             .insert_resource(PerformanceData {
                 frame_time: [0.0].into(),
+                simulation_time: [0.0].into(),
             })
             .insert_resource(PerformanceGuiSettings {
                 history_to_show: 100,
@@ -22,8 +23,9 @@ impl Plugin for PreformanceGuiPlugin {
 }
 
 #[derive(Resource, Debug)]
-struct PerformanceData {
-    frame_time: VecDeque<f32>,
+pub struct PerformanceData {
+    pub frame_time: VecDeque<f32>,
+    pub simulation_time: VecDeque<f32>,
 }
 
 #[derive(Resource)]
@@ -33,20 +35,22 @@ struct PerformanceGuiSettings {
 
 fn performance_gui(
     mut contexts: EguiContexts,
-    time: Res<Time>,
     mut performance_data: ResMut<PerformanceData>,
     mut gui_settings: ResMut<PerformanceGuiSettings>,
 ) {
-    performance_data
-        .frame_time
-        .push_back(1000.0 * time.delta_secs());
-
     while performance_data.frame_time.len() > gui_settings.history_to_show as usize {
         performance_data.frame_time.pop_front();
     }
 
+    while performance_data.simulation_time.len() > gui_settings.history_to_show as usize {
+        performance_data.simulation_time.pop_front();
+    }
+
     let average_frame_time: f32 =
         performance_data.frame_time.iter().sum::<f32>() / performance_data.frame_time.len() as f32;
+
+    let average_sim_time: f32 = performance_data.simulation_time.iter().sum::<f32>()
+        / performance_data.frame_time.len() as f32;
 
     egui::Window::new("performance").show(contexts.ctx_mut(), |ui| {
         let frame_time_plot: PlotPoints = performance_data
@@ -56,8 +60,10 @@ fn performance_gui(
             .map(|(i, frametime)| [i as f64, *frametime as f64])
             .collect();
 
-        ui.label(format!("fps: {:.0}", 1.0 / time.delta_secs()));
-        ui.label(format!("frametime: {:.1}ms", 1000.0 * time.delta_secs()));
+        ui.label(format!(
+            "frametime: {:.1}ms",
+            performance_data.frame_time[0]
+        ));
         ui.label(format!("average frametime: {:.1}ms", average_frame_time));
         ui.label("frame time plot");
 
@@ -73,6 +79,28 @@ fn performance_gui(
             .x_axis_label("frame")
             .show(ui, |plot_fn| plot_fn.line(Line::new(frame_time_plot)));
 
-        ui.add(Slider::new(&mut gui_settings.history_to_show, 0..=500).text("history to show"))
+        let simulation_time_plot: PlotPoints = performance_data
+            .simulation_time
+            .iter()
+            .enumerate()
+            .map(|(i, simtime)| [i as f64, *simtime as f64])
+            .collect();
+
+        ui.label(format!("average simtime: {:.1}ms", average_sim_time));
+        ui.label("sim time plot");
+
+        Plot::new("sim time plot")
+            .height(75.0)
+            .allow_zoom(false)
+            .allow_scroll(false)
+            .allow_drag(false)
+            .show_x(false)
+            .set_margin_fraction(egui::Vec2::new(0.0, 0.0))
+            .include_x(0.0)
+            .include_y(20.0)
+            .x_axis_label("frame")
+            .show(ui, |plot_fn| plot_fn.line(Line::new(simulation_time_plot)));
+
+        ui.add(Slider::new(&mut gui_settings.history_to_show, 1..=500).text("history to show"))
     });
 }
--- src/simulation/gravity.rs
+++ src/simulation/gravity.rs
@@ -5,14 +5,12 @@ use bevy::prelude::*;
 
 pub fn calc_grav_accel(
     mut query: Query<(&mut Acceleration, &Mass, &Transform), With<Particle>>,
-    time: Res<Time>,
     sim_settings: Res<SimSettings>,
 ) {
     let mut iter = query.iter_combinations_mut();
     while let Some([(mut accel_1, Mass(mass_1), pos_1), (mut accel_2, Mass(mass_2), pos_2)]) =
         iter.fetch_next()
     {
-        let dt = time.delta_secs();
         // a_a = (m_b/|r|^3) * r * dt * G
         let pos_1 = pos_1.translation;
         let pos_2 = pos_2.translation;
@@ -21,11 +19,19 @@ pub fn calc_grav_accel(
         if distance_sq < 1e-20 {
             continue;
         }
-        let distance = distance_sq.sqrt();
+        let mut distance = distance_sq.sqrt();
+        // if collisions are turned off, the particles get super close and start to be flung
+        // away by the huge amount of force; this helps alot
+        if !sim_settings.enable_collisions {
+            distance = distance.max(25.0);
+        }
+        // even with collisions on things maybe will get flunng
+        distance = distance.max(10.0);
+
         let distance_cubed = distance * distance * distance;
         accel_1.0 +=
-            (((sim_settings.gravity_constant * mass_2) / (distance_cubed)) * delta * dt).truncate();
+            (((sim_settings.gravity_constant * mass_2) / (distance_cubed)) * delta).truncate();
         accel_2.0 -=
-            (((sim_settings.gravity_constant * mass_1) / (distance_cubed)) * delta * dt).truncate();
+            (((sim_settings.gravity_constant * mass_1) / (distance_cubed)) * delta).truncate();
     }
 }
