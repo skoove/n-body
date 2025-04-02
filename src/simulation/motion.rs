98a621c9e39c083e0ce79d7e00ff3843b601f54a
skoove <zie@skoove.dev>
2025-04-03 07:12:01 +1000

    seperate verlet integration into a seperate function
    This will help later

--- src/simulation/motion.rs
+++ src/simulation/motion.rs
@@ -22,15 +22,28 @@ pub fn update_particle_positions(
     query.par_iter_mut().for_each(
         |(mut position, mut old_position, mut acceleration, mut previous_acceleration)| {
             let dt = time.delta_secs();
-            let velocity = position.translation - old_position.0.translation;
+            let pos = position.translation.truncate();
+            let old_pos = old_position.0.translation.truncate();
 
-            old_position.0.translation = position.translation;
+            let new_pos = verlet_integrate(dt, pos, old_pos, acceleration.0);
 
-            position.translation =
-                (position.translation.truncate() + velocity.truncate() + acceleration.0 * dt * dt)
-                    .extend(0.0);
+            old_position.0 = *position;
+            position.translation = new_pos.extend(0.0);
+
             previous_acceleration.0 = acceleration.0;
             acceleration.0 = Vec2::ZERO;
         },
     );
+}
+
+/// Returns the next position of an object from delta time, position, previous position and acceleration.
+/// If using this to actually move things, remember to update old position and to reset acceleration at the end
+pub fn verlet_integrate(
+    delta_secs: f32,
+    position: Vec2,
+    old_position: Vec2,
+    acceleration: Vec2,
+) -> Vec2 {
+    let velocity = position - old_position;
+    position + velocity + acceleration * delta_secs * delta_secs
 }
