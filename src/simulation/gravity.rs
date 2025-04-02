3b74a36e4ce7c7be3779363af4bc5f13172df821
skoove <zie@skoove.dev>
2025-04-02 11:52:17 +1000

    improve stability when no collisions

--- src/simulation/gravity.rs
+++ src/simulation/gravity.rs
@@ -1,15 +1,16 @@
-use crate::particle::{Mass, Particle};
+use crate::particle::{Mass, Particle, Radius};
 use crate::simulation::motion::Acceleration;
 use crate::simulation::SimSettings;
 use bevy::prelude::*;
 
 pub fn calc_grav_accel(
-    mut query: Query<(&mut Acceleration, &Mass, &Transform), With<Particle>>,
+    mut query: Query<(&mut Acceleration, &Mass, &Transform, &Radius), With<Particle>>,
     sim_settings: Res<SimSettings>,
 ) {
     let mut iter = query.iter_combinations_mut();
-    while let Some([(mut accel_1, Mass(mass_1), pos_1), (mut accel_2, Mass(mass_2), pos_2)]) =
-        iter.fetch_next()
+    while let Some(
+        [(mut accel_1, Mass(mass_1), pos_1, Radius(radius_1)), (mut accel_2, Mass(mass_2), pos_2, Radius(radius_2))],
+    ) = iter.fetch_next()
     {
         // a_a = (m_b/|r|^3) * r * dt * G
         let pos_1 = pos_1.translation;
@@ -20,13 +21,9 @@ pub fn calc_grav_accel(
             continue;
         }
         let mut distance = distance_sq.sqrt();
-        // if collisions are turned off, the particles get super close and start to be flung
-        // away by the huge amount of force; this helps alot
-        if !sim_settings.enable_collisions {
-            distance = distance.max(25.0);
-        }
-        // even with collisions on things maybe will get flunng
-        distance = distance.max(10.0);
+
+        // stop things being flung
+        distance = distance.max(*radius_1 + *radius_2);
 
         let distance_cubed = distance * distance * distance;
         accel_1.0 +=
