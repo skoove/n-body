4494f8fa036270ff13aa843180fa98dbd097142c
Zie Sturges <zie@sturges.com.au>
2025-04-09 10:44:17 +1000

    flake: update to unstable rust

--- flake.nix
+++ flake.nix
@@ -3,12 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, fenix }:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    fe_pkgs = fenix.packages.${system};
 
     librarys = with pkgs; [
       lld
@@ -25,10 +31,15 @@
   in {
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = with pkgs; [
-        cargo
-        clippy
+        (fe_pkgs.complete.withComponents [
+          "cargo"
+          "clippy"
+          "rust-src"
+          "rustc"
+          "rustfmt"
+          "rust-analyzer"
+        ])
         clang
-        rust-analyzer
         pkg-config
       ] ++ librarys;
 
