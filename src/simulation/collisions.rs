1e957c94e6bfdf190bb1f6740f52fc318a9a0508
Zie <zie@skoove.dev>
2025-09-08 14:58:20 +1000

    try and fix spinny

--- src/simulation/collisions.rs
+++ src/simulation/collisions.rs
@@ -1,10 +1,13 @@
-use crate::particle::{Particle, Radius};
+use crate::{
+    particle::{Particle, Radius},
+    simulation::motion::OldPosition,
+};
 use bevy::prelude::*;
 
 use super::SimSettings;
 
 pub fn calculate_collisions(
-    mut particles: Query<(&mut Transform, &Radius), With<Particle>>,
+    mut particles: Query<(&mut Transform, &OldPosition, &Radius), With<Particle>>,
     sim_settings: Res<SimSettings>,
 ) {
     if !sim_settings.enable_collisions {
@@ -12,18 +15,24 @@ pub fn calculate_collisions(
     }
     for _ in 0..sim_settings.collision_steps {
         let mut iter = particles.iter_combinations_mut();
-        while let Some([(mut pos1, Radius(radius1)), (mut pos2, Radius(radius2))]) =
-            iter.fetch_next()
+        while let Some(
+            [(mut pos1, old_pos1, Radius(radius1)), (mut pos2, old_pos2, Radius(radius2))],
+        ) = iter.fetch_next()
         {
             let distance = pos1.translation - pos2.translation;
             let distance_length = distance.length();
 
             if distance_length < radius1 + radius2 {
                 let overlap = radius1 + radius2 - distance_length;
-                let collision_normal = distance.normalize();
+
+                let velocity_1 = (pos1.translation - old_pos1.0.translation).normalize();
+                let velocity_2 = (pos2.translation - old_pos2.0.translation).normalize();
+
+                let velocity_vector = velocity_1.dot(velocity_2);
+
                 let move_distance = overlap / 2.0;
-                pos1.translation += collision_normal * move_distance;
-                pos2.translation -= collision_normal * move_distance;
+                pos1.translation += velocity_vector * move_distance;
+                pos2.translation -= velocity_vector * move_distance;
             }
         }
     }
